bff6f93e2115c16b4f7b9dfe3b12126d7dad5b83
SC-V <37342773+SC-V@users.noreply.github.com>
2023-02-05 19:51:33 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,137 @@
+import requests
+import json
+import pandas as pd
+import streamlit as st
+from io import BytesIO
+
+ORD_SHEET_ID = st.secrets["ORD_SHEET_ID"]
+URL = st.secrets["URL"]
+API_KEY = st.secrets["API_KEY"]
+STATION_ID = st.secrets["STATION_ID"]
+
+
+def read_orders_frame() -> pd.DataFrame:
+    request = requests.get(f'https://docs.google.com/spreadsheets/d/{ORD_SHEET_ID}/export?gid=0&format=csv',
+                           verify=False)
+    orders_in_csv = request.content
+    orders_to_load = pd.read_csv(BytesIO(orders_in_csv), index_col=0).reset_index()
+    print(orders_to_load)
+    return orders_to_load
+
+
+def create_order(barcode: str, comment: str, customer_address: str, fname: str, lname: str, phone: str):
+    url = f"{URL}/create?dump=eventlog"
+    payload = json.dumps({
+        "info": {
+            "operator_request_id": f"mex-magico-{barcode}",
+            "comment": comment
+        },
+        "last_mile_policy": "time_interval",
+        "source": {
+            "platform_station": {
+                "platform_id": STATION_ID
+            }
+        },
+        "destination": {
+            "type": "custom_location",
+            "custom_location": {
+                "details": {
+                    "full_address": customer_address.replace("#", "").replace("º", "")
+                }
+            }
+        },
+        "items": [
+            {
+                "count": 1,
+                "name": "Order",
+                "article": f"{barcode}",
+                "barcode": f"{barcode}",
+                "billing_details": {
+                    "unit_price": 0,
+                    "assessed_unit_price": 0,
+                    "currency": "MXN"
+                },
+                "physical_dims": {
+                    "predefined_volume": 800,
+                    "weight_gross": 150
+                },
+                "place_barcode": f"{barcode}"
+            }
+        ],
+        "places": [
+            {
+                "physical_dims": {
+                    "predefined_volume": 800
+                },
+                "description": "YD almacen orden",
+                "barcode": f"{barcode}"
+            }
+        ],
+        "billing_info": {
+            "payment_method": "already_paid"
+        },
+        "recipient_info": {
+            "first_name": fname,
+            "last_name": lname,
+            "phone": f"+{str(phone)}"
+        }
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f"Bearer {API_KEY}"
+    }
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload)
+    except:
+        print("Failed to create order with unknown error")
+        return "Failed to create order with unknown error", 500
+    print("CREATE")
+    print(response.headers)
+    print(response.text)
+    if response.status_code != 200:
+        return response.text, response.status_code
+    data = json.loads(response.text)
+    print(data)
+    offer_to_approve = data['offers'][0]['offer_id']
+    print(offer_to_approve)
+    url = f"{URL}/api/b2b/platform/offers/confirm"
+    payload = json.dumps({
+        "offer_id": offer_to_approve
+    })
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f"Bearer {API_KEY}"  # Set "Bearer <TOKEN> here"
+    }
+    try:
+        response = requests.request("POST", url, headers=headers, data=payload)
+    except:
+        print("Failed to approve order with unknown error")
+        return "Failed to approve order with unknown error", 500
+    print("CONFIRM")
+    print(response.headers)
+    print(response.text)
+    return response.text, response.status_code
+
+
+def load_mex_wh_orders():
+    all_orders = read_orders_frame()
+    result = []
+    for index, row in all_orders.iterrows():
+        print(f"ADDRESS: {row['Address']}")
+        response, status_code = create_order(
+            barcode=row['Barcode'],
+            comment="Yango Warehouse order",
+            customer_address=row['Address'],
+            fname=row['Recipient'],
+            lname="-",
+            phone=row['Phone'])
+        result.append([row['Address'], response, status_code])
+    parsed_addresses = pd.DataFrame(result, columns=['Address', 'Response', 'RCode'])
+    st.dataframe(parsed_addresses)
+    print(parsed_addresses)
+
+
+st.markdown(f"# Load orders")
+
+if st.sidebar.button("Upload from google sheets", type="primary"):
+    load_mex_wh_orders()
