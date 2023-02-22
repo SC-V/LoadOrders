67668d9b23fca8306da9d14728bf34cb91e2c07b
SC-V <37342773+SC-V@users.noreply.github.com>
2023-02-22 13:01:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def create_order(barcode: str, comment: str, customer_address: str, fname: str, 
     normalized_phone = normalize(phone)
     payload = json.dumps({
         "info": {
-            "operator_request_id": f"{barcode}-2022-02-21",
+            "operator_request_id": f"{barcode}",
             "comment": comment
         },
         "last_mile_policy": "time_interval",
