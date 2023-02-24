b418cffdd5300e572b56713c424bd874a37e8a37
SC-V <37342773+SC-V@users.noreply.github.com>
2023-02-24 15:58:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -144,3 +144,45 @@ st.markdown(f"# Load orders")
 st.caption(f"Add orders here, then press upload button: {LOAD_LINK}", unsafe_allow_html=True)
 if st.sidebar.button("Upload from Google sheets", type="primary"):
     load_mex_wh_orders()
+    
+interval_start, interval_end = st.select_slider(
+    'Select delivery window',
+    options=['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+             '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21',
+             '22', '23', '24'],
+    value=('00', '24'))
+
+deliver_till = "1970-01-02T00:00:00-06:00" if interval_end == '24' else f"1970-01-01T{interval_end}:00:00-06:00"
+start_routing_at = "1970-01-02T00:00:00-06:00" if interval_start == '24' else f"1970-01-01T{interval_start}:00:00-06:00"
+pickup_till = start_routing_at
+col_cour, col_unit, col_prox = st.columns(3, gap="medium")
+with col_cour:
+    couriers = st.number_input('Maximum number of couriers', value=10, min_value=0, max_value=3000, step=1)
+with col_unit:
+    units = st.number_input('Limit of orders per courier', value=50, min_value=0, max_value=500, step=1)
+with col_prox:
+    global_proximity_factor = st.number_input('Proximity factor', value=0.0, min_value=0.0, max_value=10.0, step=0.1)
+
+st.write('Delivery window from', interval_start, 'to', interval_end, "–",
+         str(int(interval_end) - int(interval_start)), "hours, Mexico time (GMT-6).\n",
+         "Routing for no more than", str(couriers), "couriers, with maximum parcels per courier of", str(units))
+
+routing_parameters = {
+    "group_id": "mexico sc",
+    "routing_settings_overrides": {
+        "delivery_guarantees": {
+            "start_routing_at": start_routing_at,
+            "pickup_till": pickup_till,
+            "deliver_till": deliver_till
+        },
+        "copy_fake_courier": {
+            "count": couriers,
+            "courier_pattern": {"units": units}
+        },
+        "global_proximity_factor": global_proximity_factor
+    }
+}
+routing_parameters = json.dumps(routing_parameters, indent=4)
+
+st.code(routing_parameters, language="json")
+
